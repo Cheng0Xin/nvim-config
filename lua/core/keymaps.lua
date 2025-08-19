7852882e5c3a769ce96347ba15d82e76a335ac5e
Cheng0Xin <71752581+Cheng0Xin@users.noreply.github.com>
2025-08-19 02:01:31 +0200

    Agda lsp configuration - Add some keybindings for using Agda interactive proving

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,13 +10,34 @@ vim.keymap.set("n", "ss", "<cmd>:w<cr>", { desc = "Save" })
 vim.keymap.set("n", "<C-x><C-s>", "<cmd>:w<cr>", { desc = "Save" })
 
 -- For `Agda`
-vim.keymap.set("n", "<leader>l", function()
-	vim.cmd("CornelisLoad")
-	vim.cmd("CornelisQuestionToMeta")
-end, { desc = "Load source and expand ?" })
-vim.keymap.set("n", "<leader>c", "<cmd>CornelisMakeCase<cr>", { desc = "Make case" })
-vim.keymap.set("n", "<leader>n", "<cmd>CornelisNextGoal<cr>", { desc = "Sel next goal" })
-vim.keymap.set("n", "<leader>p", "<cmd>CornelisPrevGoal<cr>", { desc = "Sel pre goal" })
-vim.keymap.set("n", "<leader>i", "<cmd>CornelisTypeContextInfer<cr>", { desc = "Show type" })
-vim.keymap.set("n", "<leader>a", "<cmd>CornelisAuto<cr>", { desc = "Auto" })
-vim.keymap.set("n", "<leader>d", "<cmd>CornelisTypeInfer<cr>", { desc = "Infer given type" })
+local function agda_filetype()
+	local opts = { buffer = true, noremap = true, silent = true }
+
+	vim.keymap.set("n", "<leader>l", function()
+		vim.cmd("CornelisLoad")
+		vim.cmd("CornelisQuestionToMeta")
+	end, { desc = "Load source and expand ?" }, opts)
+	vim.keymap.set("n", "<leader>c", ":CornelisMakeCase<CR>", opts)
+	vim.keymap.set("n", "<leader>r", ":CornelisRefine<CR>", opts)
+	vim.keymap.set("n", "<leader>i", ":CornelisTypeContextInfer<CR>", opts)
+	vim.keymap.set("n", "<leader>n", "<cmd>CornelisNextGoal<cr>", { desc = "Sel next goal" }, opts)
+	vim.keymap.set("n", "<leader>p", "<cmd>CornelisPrevGoal<cr>", { desc = "Sel pre goal" }, opts)
+	vim.keymap.set("n", "gd", ":CornelisGoToDefinition<CR>", opts)
+	vim.keymap.set("n", "<leader>s", ":CornelisSolve<CR>", opts)
+	vim.keymap.set("n", "<leader>a", ":CornelisAuto<CR>", opts)
+	vim.keymap.set("n", "<leader>d", "<cmd>CornelisTypeInfer<cr>", { desc = "Infer given type" }, opts)
+	vim.keymap.set("n", "<leader>,", "<cmd>CornelisGive<cr>", { desc = "Infer given type" }, opts)
+	-- vim.keymap.set("n", "<leader>,", ":CornelisTypeContext<CR>", opts)
+	-- -- vim.keymap.set("n", "<C-A>", ":CornelisInc<CR>", opts)
+	-- vim.keymap.set("n", "<C-X>", ":CornelisDec<CR>", opts)
+end
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.agda",
+	callback = agda_filetype,
+})
+
+vim.api.nvim_create_autocmd("QuitPre", {
+	pattern = "*.agda",
+	command = "CornelisCloseInfoWindows",
+})
