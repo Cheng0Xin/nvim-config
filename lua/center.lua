2ebe62551b41060d958be715501ffd12c7b1db2b
cheng.xin <claudexin@outlook.com>
2025-08-21 15:34:43 +0200

    Re.

--- lua/center.lua
+++ lua/center.lua
@@ -38,6 +38,31 @@ require("lazy").setup({
 		},
 	},
 
+	-- Clip management
+	{
+		"AckslD/nvim-neoclip.lua",
+		dependencies = {
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		config = function()
+			require("neoclip").setup()
+		end,
+	},
+
+	-- TODO: management
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+
+	-- Mark management
+	{
+		"chentoast/marks.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+
 	-- Themes, visualization
 	{ "nvim-lualine/lualine.nvim" }, -- status line
 	{ "nvim-tree/nvim-tree.lua" }, -- file browser
@@ -77,6 +102,11 @@ require("lazy").setup({
 					require("telescope.builtin").find_files()
 				end,
 				desc = "Find files",
+			},
+			{
+				"<leader> ",
+				"<cmd>:Telescope<cr>",
+				desc = "Find clips",
 			},
 		},
 		opts = {},
