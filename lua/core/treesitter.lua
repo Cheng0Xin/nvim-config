d89081f28fc8c140831ded26b1bd8f0b8f3853a4
cheng.xin <claudexin@outlook.com>
2026-05-29 16:55:57 +0200

    Fuck off AI.

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,2 +1,22 @@
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+for _, m in ipairs({
+	{ "x", "<M-o>", "an", "Expand selection" },
+	{ "x", "<M-i>", "in", "Shrink selection" },
+	{ "n", "<M-o>", "van", "Expand selection" },
+	{ "n", "<M-i>", "vin", "Shrink selection" },
+}) do
+	vim.keymap.set(m[1], m[2], m[3], {
+		remap = true,
+		silent = true,
+		desc = m[4],
+	})
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+	pattern = "*.lalrpop",
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
--- lua/plugins/language-agda.lua
+++ lua/plugins/language-agda.lua
@@ -3,39 +3,74 @@ return {
 		"isovector/cornelis",
 		name = "cornelis",
 		ft = "agda",
-		build = "stack install",
 		dependencies = { "neovimhaskell/nvim-hs.vim", "kana/vim-textobj-user" },
 		version = "*",
+		init = function()
+			vim.g.cornelis_no_agda_input = 1
+		end,
 		config = function()
-			local function agda_filetype()
-				local opts = { buffer = true, noremap = true, silent = true }
-				vim.g.cornelis_no_agda_input = 1
+			-- Keybindings
+			local opts = { buffer = true, noremap = true, silent = true }
 
-				vim.keymap.set("n", "<leader>l", function()
-					vim.cmd("CornelisLoad")
-					vim.cmd("CornelisQuestionToMeta")
-				end, { desc = "Load source and expand ?" }, opts)
-				vim.keymap.set("n", "<leader>c", ":CornelisMakeCase<CR>", opts)
-				vim.keymap.set("n", "<leader>r", ":CornelisRefine<CR>", opts)
-				vim.keymap.set("n", "<leader>i", ":CornelisTypeContextInfer<CR>", opts)
-				vim.keymap.set("n", "<leader>n", "<cmd>CornelisNextGoal<cr>", { desc = "Sel next goal" }, opts)
-				vim.keymap.set("n", "<leader>p", "<cmd>CornelisPrevGoal<cr>", { desc = "Sel pre goal" }, opts)
-				vim.keymap.set("n", "gd", ":CornelisGoToDefinition<CR>", opts)
-				vim.keymap.set("n", "<leader>s", ":CornelisSolve<CR>", opts)
-				vim.keymap.set("n", "<leader>a", ":CornelisAuto<CR>", opts)
-				vim.keymap.set("n", "<leader>d", "<cmd>CornelisTypeInfer<cr>", { desc = "Infer given type" }, opts)
-				vim.keymap.set("n", "<leader>,", "<cmd>CornelisGive<cr>", { desc = "Infer given type" }, opts)
-				-- vim.keymap.set("n", "<leader>,", ":CornelisTypeContext<CR>", opts)
-				-- -- vim.keymap.set("n", "<C-A>", ":CornelisInc<CR>", opts)
-				-- vim.keymap.set("n", "<C-X>", ":CornelisDec<CR>", opts)
-				vim.keymap.set("n", "<leader>u", "<cmd>CornelisInputSymbol<CR>", { buffer = true })
+			vim.keymap.set("n", "<leader>l", function()
+				vim.cmd("CornelisLoad")
+			end, vim.tbl_extend("force", opts, { desc = "Load source" }))
+
+			vim.keymap.set("n", "<leader>L", function()
+				vim.cmd("CornelisQuestionToMeta")
+			end, vim.tbl_extend("force", opts, { desc = "Expand ?" }))
+
+			vim.keymap.set("n", "<leader>c", "<cmd>CornelisMakeCase<CR>", opts)
+			vim.keymap.set("n", "<leader>r", "<cmd>CornelisRefine<CR>", opts)
+			vim.keymap.set("n", "<leader>i", "<cmd>CornelisTypeContextInfer<CR>", opts)
+			vim.keymap.set(
+				"n",
+				"<leader>n",
+				"<cmd>CornelisNextGoal<CR>",
+				vim.tbl_extend("force", opts, { desc = "Select next goal" })
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>p",
+				"<cmd>CornelisPrevGoal<CR>",
+				vim.tbl_extend("force", opts, { desc = "Select previous goal" })
+			)
+			vim.keymap.set("n", "gd", "<cmd>CornelisGoToDefinition<CR>", opts)
+			vim.keymap.set("n", "<leader>s", "<cmd>CornelisSolve<CR>", opts)
+			vim.keymap.set("n", "<leader>a", "<cmd>CornelisAuto<CR>", opts)
+			vim.keymap.set(
+				"n",
+				"<leader>d",
+				"<cmd>CornelisTypeInfer<CR>",
+				vim.tbl_extend("force", opts, { desc = "Infer type" })
+			)
+			vim.keymap.set("n", "<leader>,", "<cmd>CornelisGive<CR>", vim.tbl_extend("force", opts, { desc = "Give" }))
+			vim.keymap.set("n", "<leader>u", "<cmd>CornelisInputSymbol<CR>", opts)
+
+			-- Binding for math
+			local path = vim.fn.stdpath("config") .. "/dict/agda-symbols.json"
+			local file = io.open(path, "r")
+
+			if file == nil then
+				vim.notify("Invalid path:" .. path, vim.log.levels.ERROR)
+				return
 			end
 
-			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-				pattern = "*.agda",
-				callback = agda_filetype,
-			})
+			local content = file:read("*a")
+			file:close()
 
+			local ok, symbols = pcall(vim.json.decode, content)
+
+			if not ok then
+				vim.notify("Invalid agda-symbols.json", vim.log.levels.ERROR)
+				return
+			end
+
+			for trigger, symbol in pairs(symbols) do
+				vim.fn["cornelis#bind_input"](trigger, symbol)
+			end
+
+			-- Exit all the windows when leave
 			vim.api.nvim_create_autocmd("QuitPre", {
 				pattern = "*.agda",
 				command = "CornelisCloseInfoWindows",
